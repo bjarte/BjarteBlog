078e262b4e55ec1509746fd1968bea454b2b4cc4
Bjarte Aune Olsen <bjarte.aune.olsen@nho.no>
2023-01-05 16:42:38 +0100

    Core.Tests: Created new test for CategoryViewModel and deleted old test that didn't work

---
+++ tests/Core.Tests/CategoryTests.cs
@@ -0,0 +1,16 @@
+﻿using Core.Features.Category.Models;
+using Xunit;
+
+namespace Core.Tests;
+
+public class CategoryTests
+{
+    [Fact]
+    public void CategoryViewModelConstructor_WhenContentIsNull_ReturnsValidViewModel()
+    {
+        var model = new CategoryViewModel(null);
+
+        Assert.NotEmpty(model.Title);
+        Assert.NotEmpty(model.Slug);
+    }
+}
