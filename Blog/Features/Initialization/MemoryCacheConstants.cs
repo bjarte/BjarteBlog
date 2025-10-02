f86ab3ad5605b0da4acfefe18945801a2b17807b
Bjarte Aune Olsen <bjarte@auneolsen.no>
2025-10-02 13:54:48 +0200

    feat(caching): added constants for default cache expiration

---
+++ Blog/Features/Initialization/MemoryCacheConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Features.Initialization;
+
+public static class MemoryCacheConstants
+{
+    public static readonly MemoryCacheEntryOptions SlidingExpiration1Day = new()
+    {
+        SlidingExpiration = TimeSpan.FromDays(1)
+    };
+}
